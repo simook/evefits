2a686fb1d7de1f9554d29a7b8d0c74827fe4dc5c
kyle Simukka <kyle@janrain.com>
2013-05-17 09:59:05 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evefits::Application.config.secret_token = '6473699ba490dc69da2d140ae6a7499fe805d6b1e6fff733712cb7180d15633c7cc91c798cfa117ee57b19180f3c4af4bda380a24c0223959d3bb76add08d8ff'
