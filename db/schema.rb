dd8f9317e2eeb94716d36c338f491f9f55c40830
kyle Simukka <kyle@janrain.com>
2013-05-28 22:23:04 -0700

    added landing page

--- db/schema.rb
+++ db/schema.rb
@@ -180,8 +180,8 @@ ActiveRecord::Schema.define(:version => 20130528172903) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
   create_table "users_roles", :id => false, :force => true do |t|
     t.integer "user_id"
